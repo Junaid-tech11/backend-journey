b05caf65f3e6cae3c3a6c460a8350cba0e8e7468
Junaid <junaid8bp01@gmail.com>
2026-05-13 21:01:22 +0500

    Day 3 - Mini API with all concepts

--- Phase_1/practice.js
+++ Phase_1/practice.js
@@ -22,25 +22,83 @@
 // });
 
 //2
-const express = require('express');
 
+
+// const express = require('express');
+
+// const app = express();
+
+// app.use(express.json());
+
+
+// app.post('/products', (req, res) => {
+
+//     const name = req.body.name;
+//     const price = req.body.price;
+//     const category = req.body.category;
+
+//     res.json({
+//         name: name,
+//         price: price,
+//         ProductCategory: category
+//     })
+// })
+
+// app.listen(3000, () => {
+//     console.log(`Server running on http://localhost:3000`);
+
+// });
+
+
+const express = require('express');
 const app = express();
 
 app.use(express.json());
 
-
-app.post('/products', (req, res) => {
-
-    const name = req.body.name;
-    const price = req.body.price;
-    const category = req.body.category;
+app.get('/', (req, res) => {
 
     res.json({
-        name: name,
-        price: price,
-        ProductCategory: category
+        'message': 'Day 3 API',
+        'author': 'Junaid',
+    })
+
+
+})
+
+app.get('/users/:id', (req, res) => {
+    console.log(req.params);
+    res.json({
+        userId: req.params.id,
+        role: req.query.role
     })
 })
+
+app.post('/login', (req, res) => {
+    console.log(req.body);
+
+    const email = req.body.email;
+    res.json({
+
+        email: email,
+
+    });
+});
+
+
+
+app.get('/products', (req, res) => {
+    const sort = req.query.sort || 'name';
+    const limit = parseInt(req.query.limit) || 10;
+    const page = parseInt(req.query.page) || 1;
+
+    res.json({
+        message: 'Users list',
+        sort: sort,
+        limit: limit,
+        page: page
+    })
+})
+
 
 app.listen(3000, () => {
     console.log(`Server running on http://localhost:3000`);
